5c263242beb6d26d407e97502499fac9eb6a8e58
blackpost38 <blackpost38@gmail.com>
2018-09-01 23:12:10 +0900

    Init

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import DigitInput from 'react-input-digit';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { value: 38 };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e, { value }) {
+    this.setState({ value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          <DigitInput
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
